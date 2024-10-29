484d42c168748fa4995c6b6e8d0a80a1b8e328c4
toufiqulislamtanmoy <tittanmoy@gmail.com>
2024-10-29 21:51:44 +0600

    project initialization complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <h1 className="text-3xl font-bold text-red-500">This is flight booking app </h1>
+      <Button>Hello SadCn UI</Button>
+    </div>
+  );
+};
+
+export default App;
